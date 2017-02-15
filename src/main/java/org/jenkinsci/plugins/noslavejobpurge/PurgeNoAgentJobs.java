533a71dfbd786110322a20112e208f9f2870fc91
pbuckley <pbuckley4192@gmail.com>
2017-02-15 22:48:01 +0000

    Fixed another possible NPE

--- src/main/java/org/jenkinsci/plugins/noslavejobpurge/PurgeNoAgentJobs.java
+++ src/main/java/org/jenkinsci/plugins/noslavejobpurge/PurgeNoAgentJobs.java
@@ -14,50 +14,43 @@ import hudson.model.queue.CauseOfBlockage.BecauseNodeIsOffline;
 import jenkins.model.Jenkins;
 
 @Extension
-public class PurgeNoAgentJobs implements RootAction
-{
+public class PurgeNoAgentJobs implements RootAction {
 
-   public void doEndOfflineAgentJobs(final StaplerRequest request, final StaplerResponse response)
-   {
-      Queue queue = Jenkins.getInstance().getQueue();
+	public void doEndOfflineAgentJobs(final StaplerRequest request, final StaplerResponse response) {
 
-      if(queue != null){
-      	for (Item job : queue.getItems())
-      	{
-        	if (job.getCauseOfBlockage() instanceof BecauseNodeIsOffline
-               	|| job.getCauseOfBlockage() instanceof BecauseLabelIsOffline)
-         	{
-           		queue.cancel(job);
-         	}
-      	 }
+		Jenkins jenkins = Jenkins.getInstance();
+		if (jenkins != null) {
+			Queue queue = jenkins.getQueue();
+			if (queue != null) {
+				for (Item job : queue.getItems()) {
+					if (job.getCauseOfBlockage() instanceof BecauseNodeIsOffline
+							|| job.getCauseOfBlockage() instanceof BecauseLabelIsOffline) {
+						queue.cancel(job);
+					}
+				}
+			}
+		}
+
+		try {
+			response.sendRedirect2(request.getRootPath());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
-      try
-      {
-         response.sendRedirect2(request.getRootPath());
-      }
-      catch (IOException e)
-      {
-         throw new RuntimeException(e);
-      }
-   }
+	@Override
+	public String getDisplayName() {
+		return "Purge Jobs with offline Agents";
+	}
 
-   @Override
-   public String getDisplayName()
-   {
-      return "Purge Jobs with offline Agents";
-   }
+	@Override
+	public String getIconFileName() {
+		return "/images/32x32/gear2.png";
+	}
 
-   @Override
-   public String getIconFileName()
-   {
-      return "/images/32x32/gear2.png";
-   }
-
-   @Override
-   public String getUrlName()
-   {
-      return "/endofflineagentjobs";
-   }
+	@Override
+	public String getUrlName() {
+		return "/endofflineagentjobs";
+	}
 
 }
